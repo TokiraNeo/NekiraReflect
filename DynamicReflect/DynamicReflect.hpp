ebd4145a6a8d097d6ea45e1b5a35015aaff9f074
Tokira <Tokiraneko@outlook.com>
2025-07-13 17:49:08 +0800

    Updating Description

--- DynamicReflect/DynamicReflect.hpp
+++ DynamicReflect/DynamicReflect.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "TypeRegist/TypeInfoRegistry.hpp"
+
+
+/** 动态反射
+ *  TypeCollection/DynamicTypeInfo.hpp 定义了动态反射的信息存储类。
+ *  主要实现了两类：EnumTypeInfo 和 ClassTypeInfo。
+ *  同时，提供了CastDynamicTypeInfo方法，用于将 DynamicTypeInfo 转换为具体的子类。
+ *
+ *  TypeRegist/TypeInfoRegistry.hpp 提供了全局注册表TypeInfoRegistry，用于管理运行时类型信息。
+ *  其中也包含了一些注册动态反射信息的辅助函数。
+ *
+ */
--- StaticReflect/StaticReflect.cpp
+++ StaticReflect/StaticReflect.cpp
@@ -27,17 +27,21 @@ struct TStruct
 };
 
 STATIC_REFLECT_BEGIN( TStruct )
-STATIC_REFLECT_FUNCTIONS(
+STATIC_REFLECT_FUNCTIONS
+(
     STATIC_REGISTER_FUNCTION( &TStruct::Func, Func ),
     STATIC_REGISTER_FUNCTION( &TStruct::ConstFunc, ConstFunc ),
-    STATIC_REGISTER_FUNCTION( &TStruct::FuncWithArgs, FuncWithArgs ) )
-    STATIC_REFLECT_VARIABLES(
-        STATIC_REGISTER_VARIABLE( &TStruct::a, a ),
-        STATIC_REGISTER_VARIABLE( &TStruct::b, b ),
-        STATIC_REGISTER_VARIABLE( &TStruct::Name, Name ) )
-    STATIC_REFLECT_END()
+    STATIC_REGISTER_FUNCTION( &TStruct::FuncWithArgs, FuncWithArgs )
+)
+STATIC_REFLECT_VARIABLES
+(
+    STATIC_REGISTER_VARIABLE( &TStruct::a, a ),
+    STATIC_REGISTER_VARIABLE( &TStruct::b, b ),
+    STATIC_REGISTER_VARIABLE( &TStruct::Name, Name )
+)
+STATIC_REFLECT_END()
 
-    int main()
+int main()
 {
     auto TStructInfo = GetStaticTypeInfo<TStruct>();
     std::cout << "Type Name: " << TStructInfo.Name << std::endl;
